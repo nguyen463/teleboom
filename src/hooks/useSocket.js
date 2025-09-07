d50a0bb0fc5215e61ea7baa82d4b7d711e98cdf1
nguyen463 <fd988747@gmail.com>
2025-09-07 16:12:08 +0700

    Update useSocket.js

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const SOCKET_SERVER_URL = "http://localhost:3001";
+const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
 export function useSocket() {
   const [socket, setSocket] = useState(null);
