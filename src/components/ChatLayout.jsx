15d9a680171d0909d1464be093abff6d6f157906
nguyen463 <fd988747@gmail.com>
2025-09-09 20:43:07 +0700

    Update ChatLayout.jsx

--- src/components/ChatLayout.jsx
+++ src/components/ChatLayout.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { logout } from "./LoginPage"; // pastikan import logout
 import { io } from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5000";
