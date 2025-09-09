06c3d4014ad67db8627218f42eaa221afd09a829
nguyen463 <fd988747@gmail.com>
2025-09-09 20:42:34 +0700

    Update auth.js

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -1,1 +1,6 @@
 
+export const logout = () => {
+  localStorage.removeItem("chat-app-token");
+  localStorage.removeItem("chat-user");
+  window.location.href = "/login"; // arahkan ke halaman login
+};
