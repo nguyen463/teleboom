1881ff5025768b25816adc5a5b118d50b8ff123f
nguyen463 <fd988747@gmail.com>
2025-09-12 10:07:09 +0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'tailwindcss': {},  // Plugin khusus v4—ganti yang lama kalau ada 'tailwindcss'
+    '@tailwindcss/postcss': {},  // Plugin khusus v4—ganti yang lama kalau ada 'tailwindcss'
     autoprefixer: {},
   },
 };
